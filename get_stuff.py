1665a145a3629f11d23e01a9a232271a5cd5b33a
markcollier <mark.collier@csiro.au>
2018-03-05 14:18:20 +1100

    minor.

--- get_stuff.py
+++ get_stuff.py
@@ -12,8 +12,8 @@ if(igo):
     print(command)
     os.system(command)
 
-igo=True
 igo=False
+igo=True
 if(igo):
   input_files=['cafepp.json','cafepp_csiro-gfdl.json','cafepp_experiments.json','cafepp_vars.json']
   for i,file in enumerate(input_files):
@@ -29,20 +29,21 @@ if(igo):
     print(command)
     os.system(command)
 
-igo=False
 igo=True
+igo=False
 if(igo):
-  input_files=['app_funcs.py','cafepp_daily.py','cafepp_daily_assimilation_year_month.py','decadal_diag.py']
+  input_files=['app_funcs.py','cafepp_daily.py','decadal_diag.py']
   #input_files=['decadal_diag.py']
   for i,file in enumerate(input_files):
     command='scp mac599@raijin.nci.org.au:decadal/'+file+' ./'
     print(command)
     os.system(command)
 
-igo=False
 igo=True
+igo=False
 if(igo):
-  input_files=['cafepp_batch.ipynb']
+  input_files=['cafepp_batch.ipynb','qjob.csh','cafepp_daily_assimilation.json','cafepp_daily_assimilation_year_month.py']
+  input_files=['cafepp_daily_assimilation.json','cafepp_daily_assimilation_year_month.py']
   for i,file in enumerate(input_files):
     command='scp mac599@raijin.nci.org.au:decadal/paper_analysis/'+file+' ./'
     print(command)
