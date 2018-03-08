24571243e8ce77d6f0550e5ed5ac5e858ebe8cfa
markcollier <mark.collier@csiro.au>
2018-03-08 15:45:14 +1100

    minor.

--- setup_jupyter_server.bash
+++ setup_jupyter_server.bash
@@ -1,15 +1,14 @@
-cd ~mac599/decadal/paper_analysis
+#cd ~mac599/decadal/paper_analysis
 
-hostname=`hostname`
+hostname=hostname
 
 value=`nslookup $hostname | awk '{if ( $1=="Address:" && NR==5 )print$2}'`
 
-echo "Running server, use"
-echo "https://$value:9999/"
+echo "Running server, use web address:"
+echo "https://$value:9990/"
 
 . /short/v14/mac599/anaconda3/etc/profile.d/conda.sh
 
 conda activate cafepp_27_scipy
 
 jupyter notebook --certfile=mycert.pem --keyfile mykey.key
-
